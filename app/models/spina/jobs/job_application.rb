63081a096404c0213863396f923336097f09871a
Dan Mitchell <dan@danmitchell.co.uk>
2018-09-11 07:24:55 +0100

    Remove duplicate job_role association

--- app/models/spina/jobs/job_application.rb
+++ app/models/spina/jobs/job_application.rb
@@ -4,8 +4,6 @@ module Spina::Jobs
 
     has_one_attached :file
 
-    belongs_to :job_role
-
     validates :name, :phone, :email, presence: true
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
       length: { maximum: 255 }
